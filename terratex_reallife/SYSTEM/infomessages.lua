7b85c38b1718ce1c7ad39283915f8626cdaabe7e
Leon Kruger <byreaper@gmx.de>
2016-06-16 17:26:09 +0200

    infomessage edited

--- terratex_reallife/SYSTEM/infomessages.lua
+++ terratex_reallife/SYSTEM/infomessages.lua
@@ -3,9 +3,9 @@ function messageA()
 	for theKey,thePlayer in ipairs(players) do
 		if(vioGetElementData(thePlayer,"playtime"))then
 			if((vioGetElementData(thePlayer,"playtime")/60)<25)then
-				outputChatBox("Infomessage fuer Neuanfaenger:",thePlayer,255,255,0)
-				outputChatBox("Besuche auch unser Forum: "..config["maindomain"],thePlayer,255,255,0)
-				outputChatBox("Komm doch ebenfals auf unseren Teamspeak3 vorbei: "..config["teamspeak"],thePlayer,255,255,0)		
+				outputChatBox("Informationen:",thePlayer,255,255,0)
+				outputChatBox("Besuche auch unser Forum! "..config["maindomain"],thePlayer,255,255,0)
+				outputChatBox("Besuche doch auch unseren TeamSpeak! "..config["teamspeak"],thePlayer,255,255,0)		
 			end	
 			outputChatBox("Die Hilfen und allgemeinen Informationen (z.B. Regeln) des Servers findest du unter F1!",thePlayer,255,255,0)	
 		end
@@ -20,7 +20,7 @@ function messageB()
 	for theKey,thePlayer in ipairs(players) do
 		if(vioGetElementData(thePlayer,"playtime"))then
 			if((vioGetElementData(thePlayer,"playtime")/60)<25)then
-				outputChatBox("Infomessage fuer Neuanfaenger:",thePlayer,255,255,0)
+				outputChatBox("Informationen:",thePlayer,255,255,0)
 				outputChatBox("Du suchst eine Telefonummer? Ruf die Auskunft an: /call 11880",thePlayer,255,255,0)
 				outputChatBox("Bei Fragen und Problemen wende dich an unsere Admins: /report ",thePlayer,255,255,0)		
 				outputChatBox("eine Liste der Admins die Online sind erhälst du mit /admins",thePlayer,255,255,0)	
@@ -37,7 +37,7 @@ function messageC()
 	for theKey,thePlayer in ipairs(players) do
 		if(vioGetElementData(thePlayer,"playtime"))then
 			if((vioGetElementData(thePlayer,"playtime")/60)<25)then
-				outputChatBox("Infomessage fuer Neuanfaenger:",thePlayer,255,255,0)
+				outputChatBox("Informationen:",thePlayer,255,255,0)
 				outputChatBox("Nutze Alt-Gr um das Klicksystem zu Öffnen und Schließen!",thePlayer,255,255,0)
 				outputChatBox("Nutze X und L um Licht und Motor an zu machen!",thePlayer,255,255,0)	
 				outputChatBox("Rufe mit F11 die Karte auf um dich besser zu orientieren!",thePlayer,255,255,0)				
